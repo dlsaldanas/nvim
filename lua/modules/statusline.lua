39dbf7bdb6c92eca3d05bdaeb16b0e98e51e1346
dlsaldanas <dlsaldanas@falabella.cl>
2026-04-20 11:34:18 -0400

    use only one status line in vsplit

--- lua/modules/statusline.lua
+++ lua/modules/statusline.lua
@@ -1,4 +1,6 @@
 vim.o.showmode = false
+-- Make statusline global (laststatus=3) to span across all windows
+vim.o.laststatus = 3
 
 vim.pack.add({
   {
@@ -8,7 +10,40 @@ vim.pack.add({
 
 local statusline = require('mini.statusline')
 -- set use_icons to true if you have a Nerd Font
-statusline.setup({ use_icons = true })
+statusline.setup({ 
+  use_icons = true,
+  -- Set content hooks to show current buffer info dynamically
+  content = {
+    active = function()
+      -- Get current buffer info
+      local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+      local git = statusline.section_git({ trunc_width = 40 })
+      local diff = statusline.section_diff({ trunc_width = 75 })
+      local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+      local filename = statusline.section_filename({ trunc_width = 140 })
+      local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+      local location = statusline.section_location({ trunc_width = 75 })
+      local search = statusline.section_searchcount({ trunc_width = 75 })
+
+      return statusline.combine_groups({
+        { hl = mode_hl,                  strings = { mode } },
+        { hl = 'statuslineDevinfo',  strings = { git, diff, diagnostics } },
+        '%<', -- Mark general truncate point
+        { hl = 'statuslineFilename', strings = { filename } },
+        '%=', -- End left alignment
+        { hl = 'statuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl,                  strings = { search, location } },
+      })
+    end,
+    inactive = function()
+      -- Show minimal info for inactive windows (though with laststatus=3, this may not be used)
+      local filename = statusline.section_filename({ trunc_width = 140 })
+      return statusline.combine_groups({
+        { hl = 'statuslineInactive', strings = { filename } },
+      })
+    end,
+  },
+})
 
 -- You can configure sections in the statusline by overriding their
 -- default behavior. For example, here we set the section for
