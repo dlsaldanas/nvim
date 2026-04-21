73cbaec3d5658ffa845f854012c733927d400556
dlsaldanas <dlsaldanas@falabella.cl>
2026-04-21 10:47:26 -0400

    add latest recomendation for luals

--- lua/modules/lsp.lua
+++ lua/modules/lsp.lua
@@ -5,23 +5,54 @@ vim.pack.add({
 -- servers need to be installed from system package manager
 vim.lsp.enable("vtsls")
 vim.lsp.enable("denols")
+vim.lsp.enable('jsonls')
 
 vim.lsp.config('lua_ls', {
-  filetypes = { 'lua' },
-  settings = {
-    Lua = {
-      runtime = { version = 'LuaJIT' },
-      diagnostics = {
-        globals = { 'vim' },
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
       },
+      -- Make the server aware of Neovim runtime files
       workspace = {
         checkThirdParty = false,
         library = {
           vim.env.VIMRUNTIME,
+          -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+          vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library',
+          -- '${3rd}/busted/library',
         },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
       },
-      telemetry = { enable = false },
-    },
+    })
+  end,
+  settings = {
+    Lua = {},
   },
 })
 
@@ -34,11 +65,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
     -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
     -- if client:supports_method('textDocument/completion') then
-      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-      -- client.server_capabilities.completionProvider.triggerCharacters = chars
+    -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+    -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+    -- client.server_capabilities.completionProvider.triggerCharacters = chars
 
-      -- vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    -- vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
     -- end
   end,
 })
